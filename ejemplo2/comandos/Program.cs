1542c402ee1166e66bf09827123a76b6b68ca536
Juanitoprofe <112763842+Juanitoprofe@users.noreply.github.com>
2022-09-15 21:57:27 -0500

    15/09/2022

---
+++ ejemplo2/comandos/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+static void Main(string[] args)
+{
+    //writeLine presenta algo en la terminal
+    Console.WriteLine("Digite un numero");
+    //Read espera a que el usuario ingrese un valor
+    int num=Console.Read();
+    //Presentacion por pantalla
+    Console.WriteLine("El ascil es ="+num);
+}
+}
